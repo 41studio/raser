aad5a134200ac4fce095b8c6de1f1a1e39d93716
Mada Aryakusumah <mada@41studio.com>
2015-12-31 15:44:21 +0700

    add setting for gitignore and env

--- composer.rb
+++ composer.rb
@@ -32,6 +32,7 @@ gem 'mini_magick'
 gem 'carrierwave'
 gem 'carrierwave_backgrounder'
 gem 'kaminari'
+gem 'bootstrap-sass', '~> 3.3.6'
 gem 'bootstrap-kaminari-views'
 gem 'slim-rails'
 gem 'nprogress-rails'
@@ -46,6 +47,7 @@ gem 'friendly_id', '~> 5.1.0'
 gem_group :development do
   gem 'thin'
   gem 'byebug'
+  gem 'foreman'
   gem 'pry-byebug'
   gem 'letter_opener_web', '~> 1.2.0'
   gem 'rspec-rails'
@@ -58,30 +60,69 @@ gem_group :development do
   gem 'better_errors'
   gem 'binding_of_caller'
   gem 'bullet'
+  gem 'meta_request'
+  gem 'awesome_print'
+  gem 'capistrano-rails'
+  gem 'capistrano-rvm'
+  gem 'capistrano3-puma', github: "seuros/capistrano-puma"
 end
 
-environment 'config.action_mailer.delivery_method = :letter_opener', env: 'development'
+application(nil, env: "development") do<<-'RUBY'
+config.action_mailer.delivery_method = :letter_opener
+  config.after_initialize do
+    Bullet.enable = true
+    Bullet.alert = true
+    Bullet.rails_logger = true
+    Bullet.console = true
+  end
+RUBY
+end
 
-generate 'devise:install'
-generate 'friendly_id'
+create_file 'Procfile', "web: bundle exec rails server \nsidekiq: bundle exec sidekiq" 
+
+create_file "app/assets/stylesheets/bootstrap_modified.scss" do<<-'SCSS'
+@import "bootstrap-sprockets";
+@import "bootstrap";
+body { padding-top: 70px; }
+SCSS
+end
+
+create_file "app/assets/stylesheets/npprogress-custom.scss" do<<-'SCSS'
+// custom loader color
+$nprogress-color: #fff;
+
+@import 'nprogress';
+@import 'nprogress-bootstrap';
+SCSS
+end
+
+create_file '.env' do <<-FILE
+# Add account credentials and API keys here.
+# This file should be listed in .gitignore to keep your settings secret!
+# Each entry sets a local environment variable.
+# For example, setting:
+# GMAIL_USERNAME=Your_Gmail_Username
+# makes 'Your_Gmail_Username' available as ENV["GMAIL_USERNAME"]
+FILE
+end
 
 inject_into_file 'app/assets/javascripts/application.js', "//= require jquery.turbolinks\n", after: "require jquery\n"
-inject_into_file 'app/assets/javascripts/application.js', "//= require nprogress\n//= require nprogress-turbolinks\n", after: "require turbolinks\n"
-inject_into_file 'app/assets/javascripts/application.js', "//= require holder\n", after: "require nprogress-turbolinks\n"
+inject_into_file 'app/assets/javascripts/application.js', after: "require turbolinks\n" do
+  "//= require nprogress\n//= require nprogress-turbolinks\n//= require holder\n" 
+end
 
-inject_into_file 'app/assets/stylesheets/application.css', " *= require nprogress\n", after: "*= require_self\n"
-inject_into_file 'app/assets/stylesheets/application.css', " *= require nprogress-bootstrap\n", after: "*= require nprogress\n"
+inject_into_file 'app/assets/stylesheets/application.css', after: "*= require_self\n" do
+  " *= require bootstrap_modified\n  *= require npprogress-custom\n"
+end
 
-inject_into_file 'config/environments/development.rb', after: "class Application < Rails::Application \n" do <<-'RUBY'
-    config.after_initialize do
-      Bullet.enable = true
-      Bullet.alert = true
-      Bullet.rails_logger = true
-      Bullet.console = true
-    end
-  RUBY
+append_file ".gitignore" do<<-FILE
+/config/database.yml
+public/uploads
+FILE
 end
 
 after_bundle do
   git :init
+  generate 'devise:install'
+  generate 'friendly_id'
 end
